d5854e3a4d732535e7cb07acc53c6e40f771ffc1
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-18 22:05:13 +0100

    createnode

--- create.c
+++ create.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 #include "monty.h"
 /**
- *  create - creates and initializes a new node for a doubly-linked stack.
+ *  createnode - creates and initializes a new node for a doubly-linked stack.
  * @n: integer
  * Return: node
 */
-stack_t *create(int n)
+stack_t *createnode(int n)
 {
 	stack_t *nope;
 
 	nope = malloc(sizeof(stack_t));
 	if (nope == NULL)
-		error(4);
+		errrrrr(4);
 	nope->next = NULL;
 	nope->prev = NULL;
 	nope->n = n;
