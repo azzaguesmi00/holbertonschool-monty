ab88a6fb5423cbbeebf2996931564293319674ae
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-19 00:05:31 +0100

    parse

--- parse.c
+++ parse.c
@@ -5,29 +5,23 @@
  * which function to call
  * @buff: line from the file
  * @ln: line number
- * @i:  storage format. If 0 Nodes will be entered as a stack.
  * if 1 nodes will be entered as a queue.
  * Return: Returns 0 if the opcode is stack. 1 if queue.
  */
 
-const char parse_line(char *buff, int ln, int i)
+char *parse_line(char *buff, int ln)
 {
-    char *opc, *val;
-    const char *c = "\n ";
+	char *opc, *val;
+	const char *c = "\n ";
 
-    if (buff == NULL)
-        errrrrr(4);
+	if (buff == NULL)
+		errrrrr(4);
 
-    opc = strtok(buff, c);
-    if (opc == NULL)
-        return (c);
-    val = strtok(NULL, c);
+	opc = strtok(buff, c);
+	if (opc == NULL)
+		return (c);
+	val = strtok(NULL, c);
 
-    if (strcmp(opc, "stack") == 0)
-        return (0);
-    if (strcmp(opc, "queue") == 0)
-        return (1);
-
-    find_func(opc, val, ln, c);
-    return (c);
+	find_func(opc, val, ln);
+	return (c);
 }
