e8a27ce2f5ec2a5e829b1e4f2ba88b6a691064cb
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-18 13:11:23 +0100

    add top two nodes

---
+++ add2.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * add - Adds the top two elements of the stack.
+ * @stak: Pointer to a pointer pointing to top node of the stack.
+ * @linum: Interger representing the line number of of the opcode.
+ */
+void add(stack_t **stak, unsigned int linum)
+{
+	int all;
+
+	if (stak == NULL || *stak == NULL || (*stak)->next == NULL)
+		more_errrrr(8, linum, "add");
+
+	(*stak) = (*stak)->next;
+	all = (*stak)->n + (*stak)->prev->n;
+	(*stak)->n = all;
+	free(stak);
+	(*stak)->prev = NULL;
+}
