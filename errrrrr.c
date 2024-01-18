5d856b97f1c8eee300b310637622d986f38a02f6
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-18 13:39:08 +0100

    error handler

--- errrrrr.c
+++ errrrrr.c
@@ -14,36 +14,36 @@
  */
 void errrrrr(int error_code, ...)
 {
-    va_list va;
-    char *c;
-    int line_number;
+	va_list va;
+	char *c;
+	int line_number;
 
-    va_start(va, error_code);
-    switch (error_code)
-    {
-    case 1:
-        fprintf(stderr, "USAGE: monty file\n");
-        break;
-    case 2:
-        fprintf(stderr, "Error: Can't open file %s\n",
-                va_arg(va, char *));
-        break;
-    case 3:
-        line_number = va_arg(va, int);
-        c = va_arg(va, char *);
-        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, c);
-        break;
-    case 4:
-        fprintf(stderr, "Error: malloc failed\n");
-        break;
-    case 5:
-        fprintf(stderr, "L%d: usage: push integer\n", va_arg(va, int));
-        break;
-    default:
-        break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_start(va, error_code);
+	switch (error_code)
+	{
+	case 1:
+		fprintf(stderr, "USAGE: monty file\n");
+		break;
+	case 2:
+		fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(va, char *));
+		break;
+	case 3:
+		line_number = va_arg(va, int);
+		c = va_arg(va, char *);
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, c);
+		break;
+	case 4:
+		fprintf(stderr, "Error: malloc failed\n");
+		break;
+	case 5:
+		fprintf(stderr, "L%d: usage: push integer\n", va_arg(va, int));
+		break;
+	default:
+		break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
@@ -56,33 +56,33 @@ void errrrrr(int error_code, ...)
  */
 void more_errrrr(int error_code, ...)
 {
-    va_list va;
-    char *c;
-    int line_number;
+	va_list va;
+	char *c;
+	int line_number;
 
-    va_start(va, error_code);
-    switch (error_code)
-    {
-    case 6:
-        fprintf(stderr, "L%d: can't pint, stack empty\n",
-                va_arg(va, int));
-        break;
-    case 7:
-        fprintf(stderr, "L%d: can't pop an empty stack\n",
-                va_arg(va, int));
-        break;
-    case 8:
-        line_number = va_arg(va, unsigned int);
-        c = va_arg(va, char *);
-        fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, c);
-        break;
-    case 9:
-        fprintf(stderr, "L%d: division by zero\n",
-                va_arg(va, unsigned int));
-        break;
-    default:
-        break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_start(va, error_code);
+	switch (error_code)
+	{
+	case 6:
+		fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(va, int));
+		break;
+	case 7:
+		fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(va, int));
+		break;
+	case 8:
+		line_number = va_arg(va, unsigned int);
+		c = va_arg(va, char *);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, c);
+		break;
+	case 9:
+		fprintf(stderr, "L%d: division by zero\n",
+				va_arg(va, unsigned int));
+		break;
+	default:
+		break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
