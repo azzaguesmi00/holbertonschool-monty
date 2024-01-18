4a476d8611c1d3b8b2ce90a15bd97f4c32591925
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-18 13:07:56 +0100

    doesnothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void nop(stack_t **stak, unsigned int linum)
+{
+	(void)stak;
+	(void)linum;
+}
